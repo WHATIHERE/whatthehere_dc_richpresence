681e6ce65427a115ae16f3f2dd5c0c6935e80eff
JKTheRipperTH <69198341+JKTheRipperTH@users.noreply.github.com>
2023-02-07 07:58:23 +0700

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,28 +1,25 @@
-
-let playerName = GetPlayerName(PlayerId())
-let playerID = GetPlayerServerId(PlayerId());
-
 /**********************************************************
  * @INFO  [PROJECT DISCORD FIVEM RICHPRESENCE]
  * CODED BY: เดินทางไปอวกาศ#6590 & QUALITY SHOP
  * ง่ายๆสั้นๆ ^_^
  *********************************************************/
-
+let playerName = GetPlayerName(PlayerId())
+let playerID = GetPlayerServerId(PlayerId());
 // อย่าแตะต้อง หากคุณไม่รู้ว่าสิ่งนี้คืออะไร!!
 on('onClientResourceStart', async () => {
-    await Wait(Config.update_delay)
-    SetDiscordAppId(Config.ApplicationId)
-    SetRichPresence(`PowerBy:JJ COPYX?\n\Username: ${playerName}\nID: ${playerID}`);
-    SetDiscordRichPresenceAssetText(Config.text)
-    SetDiscordRichPresenceAsset(Config.Assetslogo)
-    SetDiscordRichPresenceAssetSmall(Config.LogoSmall)
-    SetDiscordRichPresenceAssetSmallText(Config.TextSmall)
-    SetDiscordRichPresenceAction(0, Config.ButtonName, Config.dc_invite)
-    SetDiscordRichPresenceAction(1, Config.ButtonName1, Config.sv_invit)
-    console.log('JJ COPY X? BY: QUALITY SHOP https://discord.gg/Jsmbn79Q4Y')
+    await Wait(1000)
+    SetDiscordAppId('') //ไอดีบอท https://discord.com/developers/applications/
+    SetRichPresence(`${playerName} [${playerID}]`)
+    SetDiscordRichPresenceAsset("ชื่อรูป")
+    SetDiscordRichPresenceAssetSmall("ชื่อรูป")
+    SetDiscordRichPresenceAssetText("ชื่อตรงไอคอน")
+    SetDiscordRichPresenceAssetSmallText("ชื่อตรงไอคอน")
+    SetDiscordRichPresenceAction(0, "JOIN SERVER", 'https://google.comx')
+    SetDiscordRichPresenceAction(1, "FAN PAGE",'https://google.comx')
+    console.log('RichPresence Started!')
 })
 /**********************************************************
  * @INFO  [PROJECT DISCORD FIVEM RICHPRESENCE]
  * CODED BY: เดินทางไปอวกาศ#6590 & QUALITY SHOP
  * ง่ายๆสั้นๆ ^_^
- *********************************************************/
+ *********************************************************/ 
